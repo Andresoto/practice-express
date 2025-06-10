430e9cc5e7ea826d2c77e358381546067f59c064
Andresoto <zuluandres27@gmail.com>
2025-06-10 15:58:06 -0500

    Create user CRUD

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
 const cors = require('cors')
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -33,6 +33,7 @@ app.get('/', (req, res) => {
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const UserService = require('../services/users.service');
 const validatorHandler = require('../middlewares/validator.handler');
-// const { createUserSchema, updateUserSchema, getUserSchema } = require('../schemas/user.schema');
+const { createUserSchema, updateUserSchema, getUserSchema } = require('../schemas/user.schema');
 
 const router = express.Router();
 const userService = new UserService();
@@ -14,6 +14,69 @@ router.get('/', async (req, res, next) => {
     next(error);
   }
 }
+);
+
+router.get('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await userService.findOne(Number(id));
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const newUser = req.body;
+      const createdUser = await userService.create(newUser);
+      res.status(201).json({
+        message: 'User created successfully',
+        user: createdUser,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const user = await userService.update(Number(id), body);
+      res.json({
+        message: 'User updated successfully',
+        user: user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const response = await userService.delete(Number(id));
+      res.json({
+        message: 'User deleted successfully',
+        response: response,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 );
 
 module.exports = router;
