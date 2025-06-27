92ba47bd0ba228ebb196c90e238372ba9eb5f8fe
Andresoto <zuluandres27@gmail.com>
2025-06-26 19:57:40 -0500

    feat: implement JWT token generation on login; return token in response

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { config } = require('../config/config');
+const jwt = require('jsonwebtoken');
+
 
 const router = express.Router();
 const passport = require('passport');
@@ -7,9 +10,16 @@ router.post('/login',
   passport.authenticate('local', { session: false }),
   (req, res, next) => {
     try {
+      const user = req.user;
+      const payload = {
+        sub: user.id,
+        role: user.role,
+      };
+      const token = jwt.sign(payload, config.jwtSecret);
       res.json({
         message: 'Logged in successfully',
         user: req.user,
+        token: token,
       });
     } catch (error) {
       next(error);
